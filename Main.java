d48213c6a58dc29dc3fad075f14d66dbb38d0fa1
rsreeja117-oss <rsreeja117@gamil.com>
2026-03-27 20:15:12 +0530

    time

--- Main.java
+++ Main.java
@@ -1,26 +1,7 @@
-
 import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
-        
-        if (m>=35){
-            
-            if (m>=90){
-                System.out.println("Grade A");
-            }
-            else if(m>=75){
-                System.out.println("Grade B");
-            }
-            else{
-                System.out.println("Grade C");
-            }
-        }
-
-        else{
-            System.out.println("Fail");
-        }
+        Scanner sc = new Scanner(ystem.in);
     }
 }
