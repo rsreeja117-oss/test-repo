2ca3075ef8a079120e9ef4b85525a18758c04864
rsreeja117-oss <rsreeja117@gamil.com>
2026-03-28 10:01:53 +0530

    changed alarm spelling

--- Main.java
+++ Main.java
@@ -1,3 +1,16 @@
-public class Main {
-    
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String day = sc.nextLine();
+
+        switch(day){
+            case "saturday","sunday" -> System.out.println("alarm at 6AM");
+            case "monday" -> System.out.println("alarm at 7AM");
+            default -> System.out.println("alarm at 8AM");
+                
+        }
+
+    }
 }
